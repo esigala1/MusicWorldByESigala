14cf8e5c350fff22ee249fd561de3e0c6beb1be9
esigala1 <sigala.effie@gmail.com>
2017-05-16 18:27:04 +0300

    Develop the app

---
+++ README.md
@@ -0,0 +1,21 @@
+Music World by Effie Sigala
+===================================
+
+Android app structure suitable for a music player.
+
+Developed for the **Udacity** Nanodegree Program **_"Android Basics Nanodegree by Google"_**.
+
+<div align="center">
+  <br><br>
+  <img src="https://c1.staticflickr.com/5/4194/34697302895_85e9ea2966.jpg" height="400" width="240"></img>
+  <br><br>
+</div>
+
+Useful Links
+--------------
+
+GitHub: [esigala1/MusicWorldByESigala](https://github.com/esigala1/MusicWorldByESigala)
+
+Udacity Course: [Android Basics - Multiscreen Apps](https://www.udacity.com/course/android-basics-multiscreen-apps--ud839)
+
+Udacity Nanodegree Program: [Android Basics Nanodegree by Google](https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803)
--- app/src/main/java/net/bplaced/esigala1/musicworld/MainActivity.java
+++ app/src/main/java/net/bplaced/esigala1/musicworld/MainActivity.java
@@ -1,7 +1,10 @@
 package net.bplaced.esigala1.musicworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Set a clicklistener on TextView "Now Playing".
+        ((TextView)findViewById(R.id.textViewNowPlaying)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new Explicit Intent to open the {@link NowPlayingActivity}.
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                // Start the new activity.
+                startActivity(nowPlayingIntent);
+            }
+        });
+        // Set a clicklistener on TextView "Songs".
+        ((TextView)findViewById(R.id.textViewSongs)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new Explicit Intent to open the {@link SongsActivity}.
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+                // Start the new activity.
+                startActivity(songsIntent);
+            }
+        });
+        // Set a clicklistener on TextView "Artist".
+        ((TextView)findViewById(R.id.textViewArtist)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new Explicit Intent to open the {@link ArtistActivity}.
+                Intent artistIntent = new Intent(MainActivity.this, ArtistActivity.class);
+                // Start the new activity.
+                startActivity(artistIntent);
+            }
+        });
+        // Set a clicklistener on TextView "Gender".
+        ((TextView)findViewById(R.id.textViewGender)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new Explicit Intent to open the {@link GenderActivity}.
+                Intent genderIntent = new Intent(MainActivity.this, GenderActivity.class);
+                // Start the new activity.
+                startActivity(genderIntent);
+            }
+        });
     }
 }
