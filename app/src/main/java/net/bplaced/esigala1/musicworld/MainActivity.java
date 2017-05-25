c13f9a0bfd92819e79eb44a111453f2f86df9118
esigala1 <sigala.effie@gmail.com>
2017-05-25 14:58:38 +0300

    Modify "Now Playing" screen.

--- app/src/main/java/net/bplaced/esigala1/musicworld/MainActivity.java
+++ app/src/main/java/net/bplaced/esigala1/musicworld/MainActivity.java
@@ -1,6 +1,7 @@
 package net.bplaced.esigala1.musicworld;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/net/bplaced/esigala1/musicworld/NowPlayingActivity.java
+++ app/src/main/java/net/bplaced/esigala1/musicworld/NowPlayingActivity.java
@@ -5,14 +5,75 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.media.MediaPlayer;  // This namespace will be used to import a MediaPlayer.
+import android.media.MediaPlayer; // This namespace will be used to import a MediaPlayer.
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NowPlayingActivity extends AppCompatActivity {
+
+    private Button btnForward, btnPause, btnPlay, btnRewind;
+    private int forwardTime = 3000;  // 3 sec forward time
+    private int backwardTime = 3000; // 3 sec backward time
+    private SeekBar seekbar;
+    private TextView tvCurrentPosition, tvDuration, tvSongName;
+    private Toast mToast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+        // Retrieve the widgets from the UI.
+        btnForward = (Button) findViewById(R.id.button_forward);
+        btnPause = (Button) findViewById(R.id.button_pause);
+        btnPlay = (Button)findViewById(R.id.button_play);
+        btnRewind = (Button)findViewById(R.id.button_rewind);
+        tvCurrentPosition = (TextView)findViewById(R.id.text_view_current_position);
+        tvDuration = (TextView)findViewById(R.id.text_view_duration);
+        tvSongName = (TextView)findViewById(R.id.text_view_song_name);
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        // Set the texts.
+        tvSongName.setText("P!nk - So What");
+        tvCurrentPosition.setText("0:00");
+        tvDuration.setText("3:45");
+
+        // Set a clicklistener on Button ">".
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Display a Toast message.
+                displayToastMessage(getResources().getString(R.string.msg_play));
+            }
+        });
+
+        // Set a clicklistener on Button "||".
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Display a Toast message.
+                displayToastMessage(getResources().getString(R.string.msg_pause));
+            }
+        });
+
+        // Set a clicklistener on Button ">>".
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Display a Toast message.
+                displayToastMessage(getResources().getString(R.string.msg_forward, forwardTime/1000));
+            }
+        });
+
+        // Set a clicklistener on Button "<<".
+        btnRewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Display a Toast message.
+                displayToastMessage(getResources().getString(R.string.msg_backward, backwardTime/1000));
+            }
+        });
+
         // Set a clicklistener on Button "Main".
         ((Button)findViewById(R.id.button_now_playing_activity_to_main)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +86,29 @@ public class NowPlayingActivity extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // If the "mToast" object is not null, then cancel it.
+        if (mToast != null){
+            mToast.cancel();
+        }
+    }
+
+    /**
+     * Display a Toast message.
+     * @param message is the message to display.
+     */
+    private void displayToastMessage(String message){
+        // If the "mToast" object is not null, then cancel it.
+        if (mToast != null){
+            mToast.cancel();
+        }
+        // Instantiate the Toast object.
+        mToast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        // Show the toast message.
+        mToast.show();
     }
 }
